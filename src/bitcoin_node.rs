ad50547f4d6aea37188ee3159d2c1b3aceacc17f
yuvicc <yuvichh01@gmail.com>
2025-07-16 19:50:31 +0530

    initialize repo

---
+++ src/bitcoin_node.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, I will be building Bitcoin Full Node using libbitcoin-kernel library and rust-bitcoin for now!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+// Bitcoin Full node in rust using libbitcoin-kernel and rust-bitcoin
+
+pub mod bitcoin_node;
